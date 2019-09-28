01dd4f9a41135c3c9a8b597d09cc6730ec5c8a13
Moizsohail <moizsohail@live.co.uk>
2019-09-28 16:13:59 +0500

    Init

---
+++ js/main.js
@@ -0,0 +1,156 @@
+(function($) {
+    number_of_tabs = 1
+    function appendTab(){
+        console.log(number_of_tabs);
+        if (number_of_tabs >= 5){
+            alert("Can't have more than 5 participants");
+            return false;
+        }
+        number_of_tabs++;
+        var participant_header = $('#temp-part-header').clone();
+        var participant_content = $('#temp-part-content').clone();
+        participant_header.toggleClass('active',false);
+        participant_content.css('max-height','0px');
+        participant_header.attr('id','');
+        participant_content.attr('id','');
+        
+        $('#part-group').append(participant_header);
+        $('#part-group').append(participant_content);
+        return true;
+    }
+    function deleteTab(el){
+        if(number_of_tabs <= 3){
+            alert("Can't Be Less Than 3 Participants")
+            return false;
+        }
+        
+        if($(this).parent().attr('id')=='temp-part-header'){
+            $(this).parent().next().next().attr('id','temp-part-header')
+            $(this).parent().next().next().next().attr('id','temp-part-content')
+        }
+        number_of_tabs--;
+
+        $(this).parent().next().remove()
+        $(this).parent().remove()
+    }
+    for(var i=0;i<2;i++){
+        appendTab();
+    }
+    $(document).on('click', '#add-participants', appendTab );
+    $(document).on('click', '.remove-button',deleteTab);
+    var form = $("#signup-form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        },
+        rules: {
+            email: {
+                email: true,
+            }
+        },
+        onfocusout: function(element) {
+            $(element).valid();
+        },
+    });
+    form.children("div").steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        enableAllSteps:true,
+        transitionEffect: "fade",
+        stepsOrientation: "vertical",
+        titleTemplate: '<div class="title"><span class="step-number">#index#</span><span class="step-text">#title#</span></div>',
+        labels: {
+            previous: 'Previous',
+            next: 'Next',
+            finish: 'Finish',
+            current: ''
+        },
+        onStepChanging: function(event, currentIndex, newIndex) {
+            if (currentIndex === 0) {
+                form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+            }
+            if (currentIndex === 1) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-0').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 2) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-1').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 3) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-2').addClass('footer-' + currentIndex + '');
+            }
+            // if(currentIndex === 4) {
+            //     form.parent().parent().parent().append('<div class="footer" style="height:752px;"></div>');
+            // }
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onFinishing: function(event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function(event, currentIndex) {
+            alert('Submited');
+        },
+        onStepChanged: function(event, currentIndex, priorIndex) {
+
+            return true;
+        }
+    });
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "Incorrect Email",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
+    });
+
+    $.dobPicker({
+        daySelector: '#birth_date',
+        monthSelector: '#birth_month',
+        yearSelector: '#birth_year',
+        dayDefault: '',
+        monthDefault: '',
+        yearDefault: '',
+        minimumAge: 0,
+        maximumAge: 120
+    });
+    var marginSlider = document.getElementById('slider-margin');
+    if (marginSlider != undefined) {
+        noUiSlider.create(marginSlider, {
+              start: [1100],
+              step: 100,
+              connect: [true, false],
+              tooltips: [true],
+              range: {
+                  'min': 100,
+                  'max': 2000
+              },
+              pips: {
+                    mode: 'values',
+                    values: [100, 2000],
+                    density: 4
+                    },
+                format: wNumb({
+                    decimals: 0,
+                    thousand: '',
+                    prefix: '$ ',
+                })
+        });
+        var marginMin = document.getElementById('value-lower'),
+	    marginMax = document.getElementById('value-upper');
+
+        marginSlider.noUiSlider.on('update', function ( values, handle ) {
+            if ( handle ) {
+                marginMax.innerHTML = values[handle];
+            } else {
+                marginMin.innerHTML = values[handle];
+            }
+        });
+    }
+})(jQuery);
